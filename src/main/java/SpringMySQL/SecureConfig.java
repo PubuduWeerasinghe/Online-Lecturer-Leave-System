3d730f81e3206f05a0105003354c2e278247719f
PubuduWeerasinghe <pubudu.lakshan7@gmail.com>
2020-03-22 22:32:42 +0530

    Initil Commit

---
+++ src/main/java/SpringMySQL/SecureConfig.java
@@ -0,0 +1,26 @@
+package SpringMySQL;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecureConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/Home","/Adminlog","/Leclog").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/Home")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
